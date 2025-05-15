08e8bf5dc3471cda568009f70259ef365c37178f
SantinoVCT <110844183+SantinoVCT@users.noreply.github.com>
2025-05-15 13:20:09 +0200

    Ajustar estilo

--- pc-gaming/src/Form/UsuarioForm.php
+++ pc-gaming/src/Form/UsuarioForm.php
@@ -5,6 +5,10 @@ namespace App\Form;
 use App\Entity\Tipo;
 use App\Entity\Usuario;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,14 +17,43 @@ class UsuarioForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $textInputCss = 'block w-full px-4 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm';
         $builder
-            ->add('Nombre')
-            ->add('Apellidos')
-            ->add('Password')
-            ->add('Email')
+            ->add('Nombre', TextType::class, [
+                'label' => 'Nombre',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
+            ->add('Apellidos', TextType::class, [
+                'label' => 'Apellidos',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
+            ->add('Password', TextType::class, [
+                'label' => 'Password',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
+            ->add('Email', TextType::class, [
+                'label' => 'Email',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
             ->add('Tipo', EntityType::class, [
+                'label' => 'Tipo',
                 'class' => Tipo::class,
-                'choice_label' => 'id',
+                'choice_label' => 'Nombre',
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
             ])
         ;
     }
