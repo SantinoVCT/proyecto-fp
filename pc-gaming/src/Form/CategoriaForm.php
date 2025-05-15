17bc4aa8a2e3934c26b1932a8c1cd8333d7eb4ff
SantiCoders <163190265+SantiCoders@users.noreply.github.com>
2025-05-15 19:09:46 +0200

    Cambio de Estilo y modificacion de las vistas de la tabla

--- pc-gaming/src/Form/CategoriaForm.php
+++ pc-gaming/src/Form/CategoriaForm.php
@@ -3,6 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Categoria;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,8 +17,15 @@ class CategoriaForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $textInputCss = 'block w-full px-4 py-2 mt-2 mb-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm';
         $builder
-            ->add('Nombre')
+            ->add('Nombre', TextType::class, [
+                'label' => 'Nombre',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
         ;
     }
 
--- pc-gaming/src/Form/ProductoForm.php
+++ pc-gaming/src/Form/ProductoForm.php
@@ -5,6 +5,11 @@ namespace App\Form;
 use App\Entity\Categoria;
 use App\Entity\Producto;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,16 +18,53 @@ class ProductoForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $textInputCss = 'block w-full px-4 py-2 mt-2 mb-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm';
+        $checkInputCss = 'block m-1 size-6';
         $builder
-            ->add('Nombre')
-            ->add('Descripcion')
-            ->add('Caracteristicas')
-            ->add('Requisitos')
-            ->add('Disponible')
-            ->add('Precio')
+            ->add('Nombre', TextType::class, [
+                'label' => 'Nombre',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
+            ->add('Descripcion', TextType::class, [
+                'label' => 'Descripcion',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
+            ->add('Caracteristicas', TextType::class, [
+                'label' => 'Caracteristicas',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
+            ->add('Requisitos', TextType::class, [
+                'label' => 'Requisitos',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
+            ->add('Disponible',null,['attr' => [
+                'class' => $checkInputCss,
+            ]])
+            ->add('Precio', NumberType::class, [
+                'label' => 'Precio',
+                'required' => false,
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
+            ])
             ->add('Categoria', EntityType::class, [
                 'class' => Categoria::class,
-                'choice_label' => 'id',
+                'choice_label' => 'Nombre',
+                'attr' => [
+                    'class' => $textInputCss,
+                ],
             ])
         ;
     }
