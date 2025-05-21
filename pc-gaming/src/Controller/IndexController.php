65854a47c532cd5aef9dbfb533c0eed0e28c3a0e
SantinoVCT <110844183+SantinoVCT@users.noreply.github.com>
2025-05-21 10:01:43 +0200

    Changes

--- pc-gaming/src/Controller/IndexController.php
+++ pc-gaming/src/Controller/IndexController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Producto;
+use App\Form\ProductoForm;
+use App\Repository\ProductoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
-    #[Route('', name: 'app_index')]
-    public function index(): Response
+    #[Route(name: 'app_index', methods: ['GET'])]
+    public function index(ProductoRepository $productoRepository): Response
     {
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'productos' => $productoRepository->findAll(),
         ]);
     }
 }
