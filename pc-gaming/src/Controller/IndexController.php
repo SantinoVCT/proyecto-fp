8743a9ff97958b805aa376191947d06e890a13de
SantiCoders <163190265+SantiCoders@users.noreply.github.com>
2025-05-30 18:09:30 +0200

    do

---
+++ pc-gaming/src/Controller/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Producto;
+use App\Form\ProductoForm;
+use App\Repository\ProductoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IndexController extends AbstractController
+{
+    #[Route(name: 'app_index', methods: ['GET'])]
+    public function index(ProductoRepository $productoRepository): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'productos' => $productoRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'out_producto_show', methods: ['GET'])]
+    public function show(Producto $producto): Response
+    {
+        return $this->render('index/show.html.twig', [
+            'producto' => $producto,
+        ]);
+    }
+}
--- pc-gaming/src/Form/UsuarioForm.php
+++ pc-gaming/src/Form/UsuarioForm.php
@@ -17,7 +17,11 @@ class UsuarioForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+<<<<<<< Updated upstream
         $textInputCss = 'block w-full px-4 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm';
+=======
+        $textInputCss = 'block w-full px-4 py-2 mt-2 mb-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm';
+>>>>>>> Stashed changes
         $builder
             ->add('Nombre', TextType::class, [
                 'label' => 'Nombre',
